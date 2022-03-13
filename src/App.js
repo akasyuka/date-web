8c596fdcb0f990217c34dfddbb8dc955cd78e1f8
akasyuka <aleksandrkasyuk@gmail.com>
2022-03-13 11:03:09 +0300

    Lesson 5 complete

--- src/App.js
+++ src/App.js
@@ -6,25 +6,54 @@ import Weather from "./components/weather";
 const API_KEY = "b8700f10b203b79cff3a2237de1ed4f2";
 
 
+
 class App extends React.Component {
+
+  state = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    sunrise: undefined,
+    sunset: undefined,
+    error: undefined
+  }
 
   gettingWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
-    const api_url = await 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},ua&appid=${API_KEY}&units=metric`);
-    const data = await api_url.json();
-    console.log(data);
 
+    if (city) {
+      const api_url = await 
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},&appid=${API_KEY}&units=metric`);
+      const data = await api_url.json();
+      console.log(data);
+
+      this.setState(
+        {
+          temp: data.main.temp,
+          city: data.name,
+          country: data.sys.country,
+          sunrise: data.sys.sunrise,
+          sunset: data.sys.sunset,
+          error: ""
+        }
+      )
+    };
   }
 
   render() {
     return (
       <div>
-        <div>Hello</div>
         <Info />
         <Form weatherMethod={this.gettingWeather}/>
-        <Weather />
+        <Weather 
+          temp={this.state.temp}
+          city={this.state.city}
+          country={this.state.country}
+          sunrise={this.state.sunrise}
+          sunset={this.state.sunset}
+          error={this.state.error}
+        />
       </div>
     );
   }
--- src/components/weather.js
+++ src/components/weather.js
@@ -4,7 +4,14 @@ class Weather extends React.Component {
     render() {
         return (
             <div>
-                <div>Погодное приложение</div>
+                { this.props.city && 
+                <div>
+                    <p>Местоположение: {this.props.city}, {this.props.country}</p>
+                    <p>Температура: {this.props.temp}</p>
+                    <p>Восход солнца: {this.props.sunrise}</p>
+                    <p>Заход солнца: {this.props.sunset}</p>
+                </div>
+                }
             </div>
         );
     }
